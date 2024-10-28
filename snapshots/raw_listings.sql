83bfa247699c819d0db972e5854f4ed07fa05eda
amansinghdalal <128581804+amansinghdalal@users.noreply.github.com>
2024-10-29 06:11:52 +1100

    last reverted

--- snapshots/raw_listings.sql
+++ snapshots/raw_listings.sql
@@ -17,7 +17,7 @@ SELECT
       HOST_ID,
       HOST_NAME,
       CASE 
-        WHEN bronze.safe_date(HOST_SINCE, 'DD-MM-YYYY') IS NOT NULL THEN safe_date(HOST_SINCE, 'DD-MM-YYYY')
+        WHEN safe_date(HOST_SINCE, 'DD-MM-YYYY') IS NOT NULL THEN safe_date(HOST_SINCE, 'DD-MM-YYYY')
         ELSE NULL 
       END AS HOST_SINCE,
       HOST_IS_SUPERHOST,
